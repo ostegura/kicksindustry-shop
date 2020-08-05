8cba43dc37125e0cab407136a9bbec968cf44ca0
ostegura <stegura99@gmail.com>
2020-08-05 04:30:42 +0300

    minor fixes

--- shop/mainShop/filters.py
+++ shop/mainShop/filters.py
@@ -1,11 +1,15 @@
 import django_filters
 
-from .models import Shoes, ShoesSize, Category
+from .models import Shoes, ShoesSize, Category, ModelSizeList
 from django.forms.widgets import TextInput
 
 
 # def get_shoesSize_qs(request):
-#     return ShoesSize.objects.all()
+#     if request is None:
+#         return ShoesSize.objects.none()
+#
+#     shoes = request.shoes.modelsizelist
+#     return shoes.shoessize_set.all()
 
 
 class ShoesFilter(django_filters.FilterSet):
@@ -17,10 +21,15 @@ class ShoesFilter(django_filters.FilterSet):
     )
     price__gt = django_filters.NumberFilter(label='Цена от:', field_name='price', lookup_expr='gte')
     price__lt = django_filters.NumberFilter(label='Цена до:', field_name='price', lookup_expr='lte')
-    model = django_filters.CharFilter(label='', field_name='model', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Поиск..'}))
+    model = django_filters.CharFilter(label='', field_name='model', lookup_expr='icontains',
+                                      widget=TextInput(attrs={'placeholder': 'Поиск..'}))
 
-    # shoes = django_filters.ModelMultipleChoiceFilter(queryset=get_shoesSize_qs, label='', field_name='model_size')
-    shoes = django_filters.CharFilter(label='Size', field_name='model_size', lookup_expr='icontains')
+    # test = django_filters.RangeFilter(field_name='price', lookup_expr='', label='Цена от и до:')
+
+    # size = django_filters.ModelMultipleChoiceFilter(queryset=get_shoesSize_qs, label='Размер:', field_name='modelsizelist__shoessize__model_size')
+    size = django_filters.CharFilter(label='Размер: ', field_name='modelsizelist__shoessize__model_size',
+                                     lookup_expr='icontains',
+                                     widget=TextInput(attrs={'placeholder': 'Введите размер..'}))
 
     ordering = django_filters.ChoiceFilter(label='Сортировать по:', choices=CHOICES, method='filter_by_price')
 
@@ -41,17 +50,5 @@ class ShoesFilter(django_filters.FilterSet):
             expression = '-sale_cnt'
         return queryset.order_by(expression)
 
-    def filter_by_size(self, queryset, name, value):
-        pass
-
-
-# # TODO add shoes size filter
-# class ShoesSizeFilter(django_filters.FilterSet):
-#
-#     # model_size = django_filters.ModelMultipleChoiceFilter(queryset=ShoesSize.objects.all(), field_name='model_size')
-#
-#     class Meta:
-#         model = ShoesSize
-#         fields = {
-#             'model_size': ['icontains'],
-#         }
+    # def filter_by_size(self, queryset, name, value):
+    #     pass
